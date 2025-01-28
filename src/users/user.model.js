d0b1c546d0a649f2082e7c58e2daf3a3e1fdc0c3
Informatica <informatica@alumno.local>
2025-01-28 11:58:39 -0600

    Se agrego el login y register de la base de datos

---
+++ src/users/user.model.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: [true, "El nombre es requerido"],
+    },
+    correo: {
+        type: String,
+        required: [true, "El correo es requerido"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "El correo es requerido"],
+    },
+    img: {
+        type: String,
+    },
+    phone: {
+        type: String,
+        minLenght: 8,
+        maxLenght: 8,
+        required: true,
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ["ADMIN_ROLE", "USER_ROLE"],
+    },
+    estado: {
+        type: Boolean,
+        default: true,
+    },
+    google: {
+        type: Boolean,
+        default: false,
+    }
+});
+
+userSchema.methods.toJSON = function (){
+    const { __v, password, ...usuario } = this.toObject();
+    usuario.uid = _id;
+    return usuario;
+}
+
+export default mongoose.model('User', userSchema);
